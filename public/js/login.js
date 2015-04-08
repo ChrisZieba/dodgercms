008d992caaa55c42f6baced860928e4dab212bfd
Chris Zieba <zieba.chris@gmail.com>
2015-04-07 22:20:04 -0700

    Make a head request to the bucket to make sure access is granted for the user

--- public/js/login.js
+++ public/js/login.js
@@ -1,96 +1,95 @@
 $(function() {
     
-	$("#login-form").submit(function(event) {
+    $("#login-form").submit(function(event) {
 
-	  	//var accessKey = $("#login-form-access-key").val();
-	  	//var accessSecret = $("#login-form-access-secret").val();
+        var accessKey = $("#login-form-access-key").val();
+        var accessSecret = $("#login-form-access-secret").val();
+        var bucket = $("#login-form-bucket").val();
+        var remember = $("#login-remember-me").is(":checked"));
 
-	  	var accessKey = 'AKIAJC2XSOXRLG3X7POQ';
-	  	var accessSecret = '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';
-
-
-
-		var sts = new AWS.STS({
-	  		accessKeyId: accessKey,
-	  		secretAccessKey: accessSecret,
-	  		sslEnabled: true,
-	  		DurationSeconds: 129600, // 36 hours
-	  		apiVersion: '2011-06-15'
-		});
-
-/*
-{
-	"Version": "2012-10-17",
-	"Id": "Policy1427772347182",
-	"Statement": [
-		{
-			"Sid": "Stmt1427772340560",
-			"Effect": "Allow",
-			"Principal": "*",
-			"Action": [
-				"s3:GetObjectVersion",
-				"s3:GetObject"
-			],
-			"NotResource": [
-				"arn:aws:s3:::entropyzx.com/admin/",
-				"arn:aws:s3:::entropyzx.com/admin/*"
-			]
-		}
-	]
-}
-
-*/
-
-	  	// This is the same policy as the user
-		var policy = '{' +
-		    '"Version": "2012-10-17",' +
-		    '"Statement": [' +
-		        '{' +
-		            '"Sid": "Stmt1427944232000",' +
-		            '"Effect": "Allow",' +
-		            '"Action": [' +
-		                '"s3:ListBucket",' +
-		                '"s3:GetObject",' +
-		                '"s3:DeleteObject",' +
-		                '"s3:PutObject"' +
-		            '],' +
-		            '"Resource": [' +
-		                '"arn:aws:s3:::manager.entropyzx.com",' +
-		                '"arn:aws:s3:::manager.entropyzx.com/*"' +
-		            ']' +
-		        '}' +
-		        // '{' +
-		        //     '"Effect": "Allow",' +
-		        //     '"Action": "sts:GetFederationToken",' +
-		        //     '"Resource": "*"' +
-		        // '}' +
-		    ']' +
-		'}';
-
-	  	var params = {
-	  		Name: 'lemonchop',
-	  		Policy: policy,
-	  		DurationSeconds: 129600, // 36 hours
-	  	};
+        var sts = new AWS.STS({
+            accessKeyId: accessKey,
+            secretAccessKey: accessSecret,
+            sslEnabled: true,
+            DurationSeconds: 129600, // 36 hours
+            apiVersion: '2011-06-15'
+        });
 
 
-		sts.getFederationToken(params, function(err, data) {
-		  if (err) {
-		  	console.log(err, err.stack);
-		  	alert(err.message);
-		  } else{
-		  	console.log(data);
-		  	// Store the federated user in local session data
-		  	sessionStorage.setItem("lemonchop-AccessKeyId", data.Credentials.AccessKeyId);
-		  	sessionStorage.setItem("lemonchop-SecretAccessKey",data.Credentials.SecretAccessKey);
-		  	sessionStorage.setItem("lemonchop-SessionToken", data.Credentials.SessionToken);
+        // This is the same policy as the user from where the token was generated
+        var policy = '{' +
+            '"Version": "2012-10-17",' +
+            '"Statement": [' +
+                '{' +
+                    '"Sid": "Stmt1427944232000",' +
+                    '"Effect": "Allow",' +
+                    '"Action": [' +
+                        '"s3:ListBucket",' +
+                        '"s3:GetObject",' +
+                        '"s3:DeleteObject",' +
+                        '"s3:PutObject"' +
+                    '],' +
+                    '"Resource": [' +
+                        '"arn:aws:s3:::manager.entropyzx.com",' +
+                        '"arn:aws:s3:::manager.entropyzx.com/*"' +
+                    ']' +
+                '}' +
+                // '{' +
+                //     '"Effect": "Allow",' +
+                //     '"Action": "sts:GetFederationToken",' +
+                //     '"Resource": "*"' +
+                // '}' +
+            ']' +
+        '}';
 
-		  	// redirect
-		  	//window.location.replace("http://localhost/www/dodgercms/admin/dashboard.html");
+        var stsParams = {
+            Name: 'dodgercms-policy',
+            Policy: policy,
+            DurationSeconds: 129600, // 36 hours
+        };
 
-		  } 
-		});
+        sts.getFederationToken(stsParams, function(err, data) {
+          if (err) {
+            console.log(err, err.stack);
+            alert("Access Denied. Please make sure the acccess key and secret are correct and try again.");
+          } else{
+            console.log(data);
+            // Store the federated user and bucket in local session data
+            sessionStorage.setItem("dodgercms-token-access-key-id", data.Credentials.AccessKeyId);
+            sessionStorage.setItem("dodgercms-token-secret-access-key", data.Credentials.SecretAccessKey);
+            sessionStorage.setItem("dodgercms-token-session-token", data.Credentials.SessionToken);
+            
 
-		event.preventDefault();
-	});
+            // If the user selected "remember me" store their access key and secret as well
+            if (remember) {
+                sessionStorage.setItem("dodgercms-access-key-id", accessKey);
+                sessionStorage.setItem("dodgercms-secret-access-key", accessSecret);
+            }
+
+            var s3 = new AWS.S3({
+                accessKeyId: data.Credentials.AccessKeyId,
+                secretAccessKey: data.Credentials.SecretAccessKey,
+                sessionToken: data.Credentials.SessionToken,
+                sslEnabled: true
+            });
+
+            var s3Params = {
+                 Bucket: bucket
+            };
+
+            s3.headBucket(s3Params, function(err, data) {
+                if (err) {
+                    alert("Access Denied. Please make sure the user attached to the access key has access to the bucket entered.");
+                } else {
+                    sessionStorage.setItem("dodgercms-bucket", bucket);
+
+                    // Success
+                    window.location.replace("http://localhost/www/dodgercms/admin/dashboard.html");
+                }
+            });
+          } 
+        });
+
+        event.preventDefault();
+    });
 });
