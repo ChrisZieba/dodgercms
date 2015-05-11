db7f264ea54ca104426e1adde9db1b49aa790339
Chris Zieba <zieba.chris@gmail.com>
2015-05-10 23:22:24 -0700

    Update init test

--- test/s3-test.js
+++ test/s3-test.js
@@ -1,5 +1,6 @@
 describe('init()', function() {
   it('should throw exception on missing arguments', function() {
     var fn = dodgercms.s3.init;
-    chai.expect(fn).to.throw(Error)
+    chai.expect(fn).to.throw('Missing arguments');
+  });
 });
