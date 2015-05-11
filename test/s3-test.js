b369652ed5114e432953b485b9c8f24ba5a2afc7
Chris Zieba <zieba.chris@gmail.com>
2015-05-10 18:52:24 -0700

    Add some tests from the s3 lib

--- test/s3-test.js
+++ test/s3-test.js
@@ -1,41 +1,5 @@
-describe('getFolders()', function() {
-  it('should return the correct list of folders', function() {
-    var objects = [
-      { Key: '/' },
-      { Key: 'test/' },
-      { Key: 'test/test/' },
-      { Key: 'test/a/b' },
-      { Key: 'test' },
-    ];
-    chai.expect(dodgercms.utils.getFolders(objects)).to.have.members(['/', 'test/', 'test/test/', 'test/a/']);
-  });
-
-  it('should return the root folder if the array is empty', function() {
-    var objects = [];
-    chai.expect(dodgercms.utils.getFolders(objects)).to.have.members(['/']);
-  });
-});
-
-describe('newFolder()', function(){
-  before(function(done) {
-    sinon
-      .stub(dodgercms.s3, 'putObject')
-      .yields(null, 'index');
-    done();
-  });
-
-  after(function(done) {
-    dodgercms.s3.putObject.restore();
-    done();
-  });
-
-  it('should match the given key', function(done){
-    dodgercms.utils.newFolder('index', 'dataBucket','siteBucket', function(err, key) {
-      if (err) {
-        return done(err);
-      }
-      chai.assert(key === 'index');
-      done();
-    });
-  });
+describe('init()', function() {
+  it('should throw exception on missing arguments', function() {
+    var fn = dodgercms.s3.init;
+    chai.expect(fn).to.throw(Error)
 });
