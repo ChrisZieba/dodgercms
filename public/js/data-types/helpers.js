9dfd97191bc721fbfbd5122beb13ad9c7d4a0a71
Matthew Levy <matthewdlevy@gmail.com>
2015-11-18 09:29:40 -0500

    added data-types files

---
+++ public/js/data-types/helpers.js
@@ -0,0 +1,46 @@
+/*
+ * Data Types Helpers
+ */
+
+Handlebars.registerHelper('before_after_split', function(id){
+  var $context = $.parseHTML(this.content);
+  var el = _.findWhere($context, {id: id});
+  $(el).find('.before, .after').remove();
+  return $(el).html();
+});
+
+Handlebars.registerHelper('checked_element', function(id){
+  var $context = $.parseHTML(this.content);
+  var el = _.findWhere($context, {id: id});
+  if(el) {
+    return ' checked';
+  }
+});
+
+Handlebars.registerHelper('multiples_text', function(id){
+  var $context = $.parseHTML(this.content);
+  var els = _.filter($context, function(element){
+    //find all elements with the id and an iterator
+    return element.id.indexOf(id) === 0;
+  });
+
+  var html = '';
+  if(els.length == 0){
+    html = '<div><input type="text" value="" id="'+id+'_0"/></div>';
+  }
+  else {
+    $(els).each(function(){
+      $(this).find('.before, .after').remove();
+      html += '<div><input type="text" value="'+$(this).html()+'" id="'+$(this).attr('id')+'"/>';
+      if($(this).attr('id') != id+'_0'){
+        //add a delete button
+        html += '<button class="button-xsmall pure-button add-multiple" data-target="'+id+'"><i class="fa fa-times"></i></button>';
+      }
+      html += '</div>';
+    }); ;
+  }
+
+  html += '<button class="button-small pure-button add-multiple" data-target="'+id+'"><i class="fa fa-plus"></i> Add another</button>';
+
+  return html;
+});
---
+++ public/js/data-types/partials.js
@@ -0,0 +1,30 @@
+/* Backbone Data Type Globals */
+var DATATYPE = Backbone.Model.extend({
+   id: {}, //unique identifier
+   name: {}, //displayed in select chooser
+   partial: {}
+});
+
+/*
+ * Data Type Partials
+ * Pass the id of the form elements to the before_after_split helper function
+ */
+ var PARTIALS_PATH = 'public/js/data-types/partials/';
+ var DATATYPES = new Backbone.Collection([
+  /* Basic HTML Template */
+  {id: 'html', name: 'HTML'},
+
+   /* Multiple Text Items (and other examples) */
+   {id: 'multiples', name: 'Multiple Text Items'} 
+
+ ], {model: DATATYPE}
+);
+
+//load the partials files
+_.each(DATATYPES.models, function(element){
+  $.get(PARTIALS_PATH + element.get('id') + '.html', function(data){
+    element.set('partial', data);
+    //register the partials for the data type forms
+    Handlebars.registerPartial('data-type-is-'+ element.get('id'), element.get('partial'));
+  });
+});
